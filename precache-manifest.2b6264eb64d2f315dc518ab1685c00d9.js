7744e45257f477ca4277f8b32624adf6aa7e1fe1
msmpunk <msm_010_27_12@hotmail.com>
2019-03-04 00:24:42 -0600

    first commit

---
+++ precache-manifest.2b6264eb64d2f315dc518ab1685c00d9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ac100e515654f2b37e80",
+    "url": "/static/js/main.ac100e51.chunk.js"
+  },
+  {
+    "revision": "704f9c8fc10099afe53d",
+    "url": "/static/js/1.704f9c8f.chunk.js"
+  },
+  {
+    "revision": "ac100e515654f2b37e80",
+    "url": "/static/css/main.5100ca2f.chunk.css"
+  },
+  {
+    "revision": "9e56531f4ad3414d1df8ce983453e8c3",
+    "url": "/index.html"
+  }
+];
